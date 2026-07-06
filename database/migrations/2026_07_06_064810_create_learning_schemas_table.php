a513f4e0ac80ddd6b1721d1ba1f310d1158341ce
Ivan Tigris <sitohang@equinoxshipping.co.id>
2026-07-06 14:59:04 +0700

    update to title

--- database/migrations/2026_07_06_064810_create_learning_schemas_table.php
+++ database/migrations/2026_07_06_064810_create_learning_schemas_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('learning_schemas', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->text('description')->nullable();
             $table->boolean('is_active')->default(true);
             $table->timestamps();
